c9f0e9836c71c30846cfeec354293d18ca238617
ARey-H39 <127042817+ARey-H39@users.noreply.github.com>
2023-05-30 22:24:18 -0500

    StockCommission: Declare variables

--- 02-StockCommission/StockCommission.java
+++ 02-StockCommission/StockCommission.java
@@ -6,6 +6,11 @@
 public class StockCommission {
 
     public static void main(String[] args) {
-        
+        double shares;
+        double pricePerShare;
+        double subTotal;
+        double commission;
+        double totalPrice;
+        String input;
     }
 }
