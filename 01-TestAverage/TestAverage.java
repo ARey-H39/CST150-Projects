85fc5ee930a676a2f6486b05748c50806156d051
ARey-H39 <127042817+ARey-H39@users.noreply.github.com>
2023-05-30 20:42:02 -0500

    TestAverage: Declare variables

--- 01-TestAverage/TestAverage.java
+++ 01-TestAverage/TestAverage.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Henry Salinas
+ */
+
 public class TestAverage {
     public static void main (String args[]) {
-
+        String studentName;
+        double scoreOne;
+        double scoreTwo;
+        double scoreThree;
+        double total;
+        double average;
     }
 }
