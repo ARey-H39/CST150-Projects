eb28f462b860f0002338c40536e8e2e3050be9d1
ARey-H39 <127042817+ARey-H39@users.noreply.github.com>
2023-06-04 16:42:54 -0500

    BookClubPoints: Get user input

--- 04-BookClubPoints/BookClubPoints.java
+++ 04-BookClubPoints/BookClubPoints.java
@@ -10,5 +10,20 @@ import javax.swing.JOptionPane;
     public static void main(String[] args) {
         // Variable where input will be stored
         int booksPurchased;
+
+        // Variable to allow user input from dialog
+        String input;
+
+        // Display dialog box and ask for user input
+        input = JOptionPane.showInputDialog(
+                "Book Club Points\n" +
+                "Check how many points you have earned by the end of the month.\n\n" +
+                "How many books have you purchased this month:"
+        );
+        // Parse input from dialog into integer
+        booksPurchased = Integer.parseInt(input);
+
+        // End dialogs
+        System.exit(0);
     }
  }
