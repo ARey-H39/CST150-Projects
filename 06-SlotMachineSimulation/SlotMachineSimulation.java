d8e1431b6f04332aa7a8a3d26a376be697e0ac71
ARey-H39 <127042817+ARey-H39@users.noreply.github.com>
2023-06-27 20:28:46 -0500

    Add input dialog that initializes the program
    The program first asks user to enter the amount of money they are
    instering into the machine, as if it were an actual slot machine.

--- 06-SlotMachineSimulation/SlotMachineSimulation.java
+++ 06-SlotMachineSimulation/SlotMachineSimulation.java
@@ -1,10 +1,23 @@
+import javax.swing.JOptionPane;
+
 /**
  * 
  * @author Henry Salinas
  */
+
 public class SlotMachineSimulation {
 
     public static void main(String[] args) {
-        
+        double moneyIn;
+
+        String input;
+
+        String message =
+            "Welcome to the Slot Machine!\n\n" +
+            "How much money would you like to insert?";
+        input = JOptionPane.showInputDialog(message);
+        moneyIn = Double.parseDouble(input);
+
+        System.exit(0);
     }
 }
